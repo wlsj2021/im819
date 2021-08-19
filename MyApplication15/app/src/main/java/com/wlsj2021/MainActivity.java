1d05e74aa0242776fa9193d9163295feb139ea02
wlsj2021 <87267648+wlsj2021@users.noreply.github.com>
2021-08-19 11:23:21 +0800

    v1
    正常

---
+++ MyApplication15/app/src/main/java/com/wlsj2021/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wlsj2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wlsj2021.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
